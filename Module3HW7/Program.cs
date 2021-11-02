654b1006999659806d3df2af3ccd396920b05f02
Mariia Zavorina <mariia.zavorina@nure.ua>
2021-11-03 00:37:12 +0200

    Add project files.

---
+++ Module3HW7/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Module3HW7
+{
+    public class Program
+    {
+        public static int Fibonacci(int n, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+            {
+                return -1;
+            }
+
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1, token) + Fibonacci(n - 2, token);
+            }
+        }
+
+        public static void FibonacciNumber()
+        {
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+            for (var i = 0; ; i++)
+            {
+                var task = Task.Run(() => Fibonacci(i, token));
+
+                if (task.Wait(TimeSpan.FromSeconds(10)))
+                {
+                    Console.WriteLine(task.Result);
+                }
+                else
+                {
+                    source.Cancel();
+                    Console.WriteLine("Cancel");
+                    break;
+                }
+            }
+        }
+
+        private static async Task Main(string[] args)
+        {
+            Thread myThread = new Thread(new ThreadStart(FibonacciNumber));
+            var taskList = new List<Task>();
+            var tasks = new Tasks();
+            taskList.Add(Task.Run(() => tasks.Add(1, 2)));
+            taskList.Add(Task.Run(() => tasks.Sub(5, 3)));
+            taskList.Add(Task.Run(() => tasks.Multiply(2, 2)));
+            taskList.Add(Task.Run(() => tasks.Divide(5, 3)));
+
+            await Task.WhenAll(taskList);
+            myThread.Start();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Module3HW7/Tasks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW7
+{
+    public class Tasks
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine(x - y);
+        }
+
+        public void Multiply(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        public void Divide(int x, int y)
+        {
+            Console.WriteLine(x / y);
+        }
+    }
+}
